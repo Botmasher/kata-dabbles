02b32593790d307728ec81f241853dbe21d4426f
Botmasher <languagejosh@hotmail.com>
2017-04-22 09:24:07 -0400

    Finish implementing translation and proper line reading for story 1 - Bank OCR

--- bank-ocr/main.py
+++ bank-ocr/main.py
@@ -28,25 +28,44 @@ seven_segments = [ \
 #segments_map = {n: seven_segments[n] for n in range(0,len(seven_segments))}
 
 # turn any nested array of triple ints into seven segs
-def translate_into_segs (lines):
+def translate_to_segs (lines):
 	segments = [] 
 	for l in lines:
-		segsments.append([ [segs[a[0]],segs[a[1]],segs[a[2]] for a in l ])
+		for a in l:
+			segments.append([ segs[a[0]],segs[a[1]],segs[a[2]]])
 	return segments
 	# also do layout, which means rotating/cutting each three
 
-# turn any nested array of triple ints into digits
-def translate_into_digits (lines):
-	digits = []
-	for l in lines:
-		line_digits = []
-		for a in l:
-			num_ind = [ segs.index(sym) for sym in a ]
-			num = seven_segments.index(num_ind)
+# not yet implemented; not used below
+def translate_to_indices_array (segs_a):
+	return None
+
+# not yet tested; not used below
+def translate_to_indices (a):
+	if a in seven_segments:
+		return seven_segments.index(a)
+	else:
+		return None
+
+# turn nested arrays of triple segments into digits
+def translate_to_digits (line):
+	line_digits = []
+	print_digits = ''
+	for a in line:
+		# find indices of seg pattern then find digit corresponding to that pattern
+		try:
+			seg_indices = [ segs.index(a[0]),segs.index(a[1]),segs.index(a[2]) ]
+			num = seven_segments.index(seg_indices)
+			# store in both array and string
 			line_digits.append(num)
-		digits.append(line_digits)
-	return digits
-	# also do printing layout (convert to string and print)
+			print_digits += str(num)
+		# not an array of 3 segments
+		except:
+			pass
+	# return as both array and string
+	#return (line_digits, print_digits)
+	# just return string since only this implemented below
+	return print_digits
 
 # open file and read lines
 class ReadWriteFile:
@@ -55,52 +74,42 @@ class ReadWriteFile:
 		self.out = out
 	
 	def output (self):
-		found_lines = []
-		count = 0
 		nums = []
 		# write to another file
 		with open(self.out, "w") as fout, open(self.src, 'r') as fin:
+			found_lineset = []
 			for l in fin:
-				count += 1
-				if count not in found_lines and len(l)>0 and l[0:3] in segs:
-					l_len = len(fin[count])
-					if l_len == len(fin[count+1]) and l_len == len(fin[count+2]):
-						found_lines.append(count)
-						found_lines.append(count + 1)
-						found_lines.append(count + 2)
-						line_nums = []
+				if len(l)>2 and l[0:3] in segs:
+					# /!\ TODO check for \n or other characters; test len matches
+					found_lineset.append(l)
+					# turn 3 lines of segs into a single digit row
+					if len(found_lineset)>2:
+						line_segs = []
 						# rip each line in threes
-						for i in range (0,int((len(l)-1)/3):
+						for i in range ( 0, int(len(l)/3) ):
 							# store index in segs
 							a = []
 							# three from line+0 -> index in segs -> a[0]
-							a.append ( l[i*3:(i+1)*3] )
+							a.append ( found_lineset[0] [i*3:(i+1)*3] )
 							# three from line+1 -> index in segs -> a[1]
-							a.append ( fin[count+1] [i*3:(i+1)*3] )
+							a.append ( found_lineset[1] [i*3:(i+1)*3] )
 							# three from line+2 -> index in segs -> a[2]
-							a.append ( fin[count+2] [i*3:(i+1)*3] )
-							# a[0:3] -> nested into line[a]
-							line_nums.append (a)
+							a.append ( found_lineset[2] [i*3:(i+1)*3] )
+							# a[0:3] -> nested into [line[a]]
+							line_segs.append (a)
+						# clear this set of 3 lines since digit row
+						found_lineset = []
 						# append line[a] -> all_nums_a[]
-						nums.append (line_nums)
-				# print per line
-				fout.write ( "%s : %s\n" % (l.rstrip('\n'), found_numline) )
-			# now your all_nums_a can be switched back and forth
-			translate_to_digits(nums)
-			translate_to_numbers(nums)
+						# translate set of 3 lines with valid segments
+						digit_line = translate_to_digits (line_segs)
+						fout.write(digit_line+"\n")
+						# store this line in all numbers found
+						nums.append (digit_line)
 			# write something after iterating
-			fout.write ( "Final finding :  %s" % (found_numline) )
-		# file automatically closes after with?
+			print(nums)
+		# file automatically closes after with
+
 txt_in = 'input.txt'
 txt_out = 'output.txt'
 txt_reader = ReadWriteFile(txt_in,txt_out)
 txt_reader.output()
-# - identify chunks (first line with only pattern [0] or [1])
-# - cut chunks at last line (3)
-# - hold chunk in memory while cutting top,mid,bottom line into n subsegs
-# - verify that the subsegs line up
-# - combine subsegs into segs
-# - turn segs into array
-# - find array in seven_segments -> that index is the digit
-# - return int(string) of all digits in that chunk
-# - go thru file until last chunk completed
